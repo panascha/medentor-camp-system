fe76951ae731225423903308c98f8f7d11529584
panascha <panasch@kkumail.com>
2026-04-16 01:51:45 +0700

    first structure for socrative

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,4 +5,13 @@ const CONFIG = {
     // รหัสลับสำหรับยืนยันตัวตนกับ Apps Script
     syncKey: "MEDENTOR_SECRET_2026",
     fbSecret: "GFPzk3iqvQr2J5nh1fmAkCJQ1iSkyhn4lZU1YOpK"
+    
+};
+
+const SUBJECTS = {
+    physic: "ฟิสิกส์",
+    chemistry: "เคมี",
+    biology: "ชีววิทยา",
+    introdent: "Intro to Dent",
+    intromed: "Intro to Med"
 };
