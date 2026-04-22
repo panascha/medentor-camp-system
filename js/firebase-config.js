2d0197c7b2587e51530ad5e9ad42ebf06806527e
panascha <panasch@kkumail.com>
2026-04-22 12:38:21 +0700

    tutor edit schedule

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     firebaseURL: "https://medentor-response-project-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    appscriptUrl: "https://script.google.com/macros/s/AKfycbxYGVH3QSJcPc4XWKaZV2etm-HP_LJFckTeJ2rigy7wa4OjxZTGDttoahV4_oYRIbzn/exec",
+    appscriptUrl: "https://script.google.com/macros/s/AKfycbyHGBSheuhfgu3bgUVxEiWd58tU9WvzUB8RchDtGDN06kZU1gt0GQCq8u8wl5RvutLf/exec",
     campYear: "2026",
     // รหัสลับสำหรับยืนยันตัวตนกับ Apps Script
     syncKey: "MEDENTOR_SECRET_2026",
